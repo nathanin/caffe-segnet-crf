1f46a6b198582f04879baafd9fc95b975a800a98
NathanIng <ing.nathany@gmail.com>
2017-05-10 22:15:33 -0700

    update notebook

--- notebooks/train.sh
+++ notebooks/train.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-cf=/home/nathan/caffe-segnet-crf/build/tools
+cf=/Users/nathaning/software/caffe-segnet-crf/build/tools
 
 $cf/caffe train \
 --solver ./solver-SegNet-CRFRNN.prototxt \
---weights /home/nathan/mzmo/analysis/nuclear_seg/segcrfrnn_raw_iter_10000.caffemodel
+# --weights /home/nathan/mzmo/analysis/nuclear_seg/segcrfrnn_raw_iter_10000.caffemodel
