ba590a9dabd9079d3bf89bd6c85f66b57ac4374d
nathan <ing.nathany@gmail.com>
2017-05-11 09:59:44 -0700

    premerge

--- notebooks/train.sh
+++ notebooks/train.sh
@@ -6,4 +6,4 @@ cf=/home/nathan/caffe-segnet-crf/build/tools
 
 $cf/caffe train \
 --solver ./solver-SegNet-CRFRNN.prototxt \
---weights /home/nathan/mzmo/analysis/nuclear_seg/segcrfrnn_raw_iter_10000.caffemodel
+# --weights /home/nathan/histo-seg/semantic-pca/weights/seg_0.8.1/raw_iter_50000.caffemodel
