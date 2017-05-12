9fec86d4ff713492ff41512048508d470e55a0ae
nathan <ing.nathany@gmail.com>
2017-05-11 18:19:58 -0700

    add two stage training script

--- notebooks/train.sh
+++ notebooks/train.sh
@@ -7,5 +7,9 @@ cf=/home/nathan/caffe-segnet-crf/build/tools
 # cf=/home/nathan/caffe-segnet-cudnn5/build/tools
 
 $cf/caffe train \
+--solver ./solver-SegNet.prototxt
+
+
+$cf/caffe train \
 --solver ./solver-SegNet-CRFRNN.prototxt \
---weights /home/nathan/mzmo/analysis/nuclear_seg/weights/crfrnn_iter_16783.caffemodel
+--weights /home/nathan/mzmo/analysis/nuclear_seg/weights/segnet_iter_25000.caffemodel
