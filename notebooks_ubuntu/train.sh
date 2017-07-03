01e78061a6d711f5c83c0534f2818e5e99fd6a87
nathan <ing.nathany@gmail.com>
2017-07-03 12:37:04 -0700

    Fixed bug in DenseImageDataLayer

--- notebooks_ubuntu/train.sh
+++ notebooks_ubuntu/train.sh
@@ -7,12 +7,13 @@ cf=/home/nathan/caffe-segnet-crf/build/tools
 $cf/caffe train \
 --solver ./models/solver-SegNet.prototxt
 
-# python ./compute_bn_statistics.py \
-# --out_dir=/Users/nathaning/databases/nuclei/ \
-# --train_model=./models/train-segnet-small-128x128.prototxt \
-# --weights=/Users/nathaning/databases/nuclei/segnet_iter_15000.caffemodel \
-# --out_name=norm_segnet_iter_15000.caffemodel
 
 $cf/caffe train \
 --solver ./models/solver-SegNet-CRFRNN.prototxt \
---weights /home/nathan/histo-seg/semantic-ccrcc/weights/segnet-crfrnn/segnet_iter_75000.caffemodel
+--weights /home/nathan/histo-seg/semantic-ccrcc/weights/segnet-crfrnn/segnet_iter_25000.caffemodel
+
+# python ./compute_bn_statistics.py \
+# --out_dir=/Users/nathaning/databases/nuclei/ \
+# --train_model=./models/train-SegNet-CRFRNN.prototxt \
+# --weights=/Users/nathaning/histo-seg/semantic-ccrcc/segnet-crfrnn/crfrnn_iter_15000.caffemodel \
+# --out_name=norm_segnet_iter_15000.caffemodel
